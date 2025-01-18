297d3ca83da0067f11d48d37b7dc50c3b14109de
logeish06 <91660505+logeish06@users.noreply.github.com>
2025-01-18 22:15:58 +0800

    update api

--- coindesk_api_automation/src/test/java/runners/TestRunner.java
+++ coindesk_api_automation/src/test/java/runners/TestRunner.java
@@ -9,6 +9,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty:target/cucumber/cucumber.txt",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"html:target/cucumber/report.html",
 				"json:target/cucumber/cucumber.json",
 				"com.api.utils.MyTestListener"
